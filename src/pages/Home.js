eb94052c017e63e511385de79340daf196040e4d
inkersal501 <inkersal501@gmail.com>
2024-10-18 23:03:16 +0530

    chatscreen basics completed

---
+++ src/pages/Home.js
@@ -0,0 +1,36 @@
+
+import React, {useState} from 'react';
+import Sidebar from '../components/Sidebar/Sidebar';
+import ChatScreen from '../components/ChatScreen/ChatScreen';
+import { Box } from '@mui/material';
+
+const drawerWidth = 250;
+
+function Home() {
+  const temp = JSON.parse(localStorage.getItem("BotAiTempData"));
+  const [newchat, setNewChat] = useState(()=>{
+    if(temp && temp.length>0){
+      return false;
+    }else{
+      return true;
+    }
+  });
+  
+   
+  return (
+    <div>
+      <Box sx={{ display: 'flex', height: '100vh' }}> 
+
+        <Sidebar />
+        <Sidebar newchat={newchat} setNewChat={setNewChat}/>
+
+        <Box
+          component="main" backgroundColor='#f5f5f5'
+        >
+          <ChatScreen /> 
+          <ChatScreen newchat={newchat} setNewChat={setNewChat}/> 
+      </Box>
+    </Box>
+    </div>
+  )
+  export default Home;
