28c3270733f803d14ad915718e0f2f722c1bed6f
inkersal501 <inkersal501@gmail.com>
2024-10-19 17:36:57 +0530

    merge commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import Sidebar from '../components/Sidebar/Sidebar';
 import ChatScreen from '../components/ChatScreen/ChatScreen';
 import { Box } from '@mui/material';
 
-const drawerWidth = 250;
+// const drawerWidth = 250;
 
 function Home() {
   const temp = JSON.parse(localStorage.getItem("BotAiTempData"));
@@ -20,17 +20,16 @@ function Home() {
   return (
     <div>
       <Box sx={{ display: 'flex', height: '100vh' }}> 
-
-        <Sidebar />
+ 
         <Sidebar newchat={newchat} setNewChat={setNewChat}/>
 
         <Box
           component="main" backgroundColor='#f5f5f5'
-        >
-          <ChatScreen /> 
+        > 
           <ChatScreen newchat={newchat} setNewChat={setNewChat}/> 
       </Box>
     </Box>
     </div>
   )
+}
   export default Home;
